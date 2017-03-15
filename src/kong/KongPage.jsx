5984ef3c675a6abd86ff04bd95a75b94ea1cdc00
Tamba Monrose <tamba@mit.edu>
2017-03-15 23:12:04 +0000

    Was able to center things and add clear div under parallax things.

--- src/kong/KongPage.jsx
+++ src/kong/KongPage.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 
 import './kong.css';
 
+// import ReactResizeDetector from 'react-resize-detector';
+import $ from "jquery";
+
 import zeroth from './images/0.png';
 import first from './images/1.png';
 import second from './images/2.png';
@@ -10,17 +13,27 @@ import fourth from './images/4.png';
 import fifth from './images/5.png';
 import sixth from './images/6.png';
 
-
 import zeroth2 from './images2/kong_Military.png';
 import first2 from './images2/kong_Water.png';
 import second2 from './images2/kong_Front.png';
 import third2 from './images2/kong_3rdBackMost.png';
 import fourth2 from './images2/kong_2ndBackMost.png';
-import fifth2 from './images2/kong_BackMost.png';
-import sixth2 from './images2/kong_Kong.png';
+import fifth2 from './images2/kong_Kong.png';
+import sixth2 from './images2/kong_BackMost.png';
 import seventh2 from './images2/kong_Sky.png';
 
+import koth from './images2/koth.png';
+
 class KongPage extends React.Component {
+  
+  componentDidMount() {
+    $(window).resize(function() {
+      console.log(window.innerWidth);
+      // console.log('...' + document.documentElement.clientWidth);
+      
+      $('#parallax__cover').css('left', 1500 - window.innerWidth/2+'px')
+    });
+  }
   
   get backgrounds() {
     return [...Array(3)].map((background, i) => (
@@ -41,24 +54,41 @@ class KongPage extends React.Component {
   }
   
   get content2() {
-    return [zeroth2, first2, second2, third2, fourth2, fifth2, sixth2, seventh2].reverse();
+    return [seventh2, sixth2, fifth2, fourth2, third2, second2, first2];
   }
   
   get parallaxes2() {
     return this.content2.map((para, i) => (
-      <div className={"parallax__layer parallax__layer__"+(i+1)}>
+      <div className={"parallax__layer parallax__layer__"+(i)}>
         <img src={para}/>
       </div>
     ));
   }
   
+  get width() {
+    console.log(window.innerWidth);
+    return {width: window.innerWidth};
+    // if (window.innerWidth >= 925) return;
+    
+    
+  }
+  
+  
+  
+  
   render() {
+    console.log($)
     return (
       <div className="home-content">
         {this.backgrounds}
         <div className="parallax">
           {this.parallaxes2}
-          <div className="parallax__cover"></div>
+          <div className={"parallax__layer parallax__layer__"+(7)}>
+            <img src={zeroth2}/>
+            <div id="parallax__cover">
+              <img src={koth}/>
+            </div>
+          </div>
         </div>
       </div>
     );
